46d0f6a2fb3ffb1e3c4906bcfafabf4cb924a290
Tacianojr07 <t.juniior13@gmail.com>
2023-05-21 15:19:31 -0300

    alteração nos campo obrigatorios no validation

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -3,6 +3,6 @@ import * as yup from "yup";
 export const userValidation = yup.object({
     name: yup.string().required(),
     email: yup.string().required().email(),
-    password: yup.string().required().min(6),
-    phone: yup.number().min(0).max(999).nullable()
+    passwrod: yup.string().required().min(6),
+    phone: yup.string().nullable()
 })
