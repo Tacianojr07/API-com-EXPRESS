6fa943dbbce4c04c0e0edeab86d5034f2ced74e0
Tacianojr07 <t.juniior13@gmail.com>
2023-05-21 14:12:57 -0300

    criando criptrografia para senha

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,11 @@
+import bcrypt from "bcrypt";
 import { createUser } from "../repositorys/user.respository"
 import { Request, Response } from "express";
 
 export const create = async(req: Request, res: Response) => {
     try {
+        const hashPassword = await bcrypt.hash(req.body.passwrod,10);
+        req.body.passwrod = hashPassword;
         const user = await createUser(req.body);
         res.status(200).send(user)
     } catch (e) {
--- src/repositorys/user.respository.ts
+++ src/repositorys/user.respository.ts
@@ -3,6 +3,15 @@ import { prisma } from "../services/prisma"
 export const createUser = async (data: any) => {
     const user = await prisma.user.create({
         data,
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            passwrod: false,
+            phone: true,
+            createdAt: true,
+            updatedAt: true
+        }
     });
     return user
 };
