ff928e4af68b079cb9e7faaa1eea135f6fc219b7
Tacianojr07 <t.juniior13@gmail.com>
2023-05-21 13:38:51 -0300

    primeira requisição criada:  POST

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,1 +1,11 @@
-import { } from "../repositorys/user.respository"
+import { createUser } from "../repositorys/user.respository"
+import { Request, Response } from "express";
+
+export const create = async(req: Request, res: Response) => {
+    try {
+        const user = await createUser(req.body);
+        res.status(200).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+}
